d55c511d9e0ca5d69af2d69a277cff58be0ddf5a
Hammy <sgoudham@gmail.com>
2020-11-21 13:15:33 +0000

    Delete classes

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.security.auth.login.LoginException;
-
-public class Main {
-    public static void main(String[] args) throws LoginException {
-        // Overwatch overwatch = new Overwatch();
-        // Player player = overwatch.getPlayerStats();
-        new Winston();
-    }
-}
