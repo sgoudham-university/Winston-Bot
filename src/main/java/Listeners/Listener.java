632785381e41c803955d1d0e0e67da566d871146
Hammy <sgoudham@gmail.com>
2020-11-21 13:16:42 +0000

    Refactoring

--- src/main/java/Listeners/Listener.java
+++ src/main/java/Listeners/Listener.java
@@ -1,7 +1,10 @@
 package Listeners;
 
+import Winston.CommandManager;
+import Winston.Config;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +16,9 @@ import java.util.Random;
 
 public class Listener extends ListenerAdapter {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
-    private static final List<String> voiceLines = new ArrayList<>();
+    private final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
+    private final CommandManager commandManager = new CommandManager();
+    private final List<String> voiceLines = new ArrayList<>();
 
 
     public Listener() {
@@ -28,17 +32,32 @@ public class Listener extends ListenerAdapter {
     }
 
     @Override
-    public void onReady(ReadyEvent event) {
+    public void onReady(final ReadyEvent event) {
         LOGGER.info("{} is ready", event.getJDA().getSelfUser().getAsTag());
     }
 
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        int random = new Random().nextInt(voiceLines.size());
-        String message = event.getMessage().getContentRaw();
-        String randomVoiceLine = voiceLines.get(random);
+    public void onGuildMessageReceived(final GuildMessageReceivedEvent event) {
+        if (event.getAuthor().isBot() || event.isWebhookMessage()) {
+            return;
+        }
 
-        if (message.toLowerCase().contains("winston") && !event.getAuthor().isBot()) {
+        if (event.getMessage().getContentRaw().startsWith(Config.get("PREFIX"))) {
+            try {
+                commandManager.handle(event);
+            } catch (final Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onMessageReceived(final MessageReceivedEvent event) {
+        final int random = new Random().nextInt(voiceLines.size());
+        final String message = event.getMessage().getContentRaw();
+        final String randomVoiceLine = voiceLines.get(random);
+
+        if (message.toLowerCase().contains("winston")) {
             event.getChannel().sendMessage(event.getAuthor().getAsMention() + " " + randomVoiceLine).queue();
             LOGGER.info("Voice Line Sent! | Voice Line That Was Chosen: '{}'", randomVoiceLine);
         }
