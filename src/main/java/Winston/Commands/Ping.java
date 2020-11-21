22668c80823fdc6df31b68cfd8b248381311d782
Hammy <sgoudham@gmail.com>
2020-11-21 13:30:34 +0000

    Add Ping Command

---
+++ src/main/java/Winston/Commands/Ping.java
@@ -0,0 +1,29 @@
+package Winston.Commands;
+
+import Command.CommandContext;
+import Command.ICommand;
+import Listeners.Listener;
+import net.dv8tion.jda.api.JDA;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Ping implements ICommand {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
+
+    @Override
+    public void handle(CommandContext ctx) {
+        JDA jda = ctx.getJDA();
+
+        jda.getRestPing().queue(
+                ping -> ctx.getChannel()
+                        .sendMessageFormat("Reset ping: %sms\nWS ping: %sms", ping, jda.getGatewayPing()).queue()
+        );
+        LOGGER.info("Ping Command Sent!");
+    }
+
+    @Override
+    public String getName() {
+        return "ping";
+    }
+}
