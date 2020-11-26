f80e512d86ef766bc8cc864d33fece63f617ba82
Hammy <sgoudham@gmail.com>
2020-11-26 17:20:14 +0000

    Custom Exception for when hero is not found

---
+++ src/main/java/Exceptions/HeroNotFoundException.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+public class HeroNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 6421798437058703463L;
+
+    public HeroNotFoundException(String message) {
+        super(message);
+    }
+}
