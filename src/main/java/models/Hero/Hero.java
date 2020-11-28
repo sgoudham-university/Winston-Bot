ee465d9370b3acdcb4a28c1f3031bfdba2b457ac
Hammy <sgoudham@gmail.com>
2020-11-28 22:12:31 +0000

    Rename packages

--- src/main/java/models/Hero/Hero.java
+++ src/main/java/models/Hero/Hero.java
@@ -1,4 +1,4 @@
-package Models.Hero;
+package models.Hero;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
--- src/main/java/models/Hero/Story.java
+++ src/main/java/models/Hero/Story.java
@@ -1,4 +1,4 @@
-package Models.Hero;
+package models.Hero;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
