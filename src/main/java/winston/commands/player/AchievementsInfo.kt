3a42e596c7a43f9bf0668054d62eac1939b00b13
Hammy <sgoudham@gmail.com>
2020-12-19 04:06:59 +0000

    Deprecate Achievements Command

--- src/main/java/winston/commands/player/AchievementsInfo.kt
+++ src/main/java/winston/commands/player/AchievementsInfo.kt
@@ -30,51 +30,87 @@ class AchievementsInfo : ICommand {
 
         buildAchievementEmbeds(player, pages, ctx, args[0])
 
-        ctx.event.channel.sendMessage((pages[0].content as MessageEmbed)).queue { success: Message? -> Pages.paginate(success, pages, 60, TimeUnit.SECONDS, 2) }
+        ctx.event.channel.sendMessage((pages[0].content as MessageEmbed))
+            .queue { success: Message? -> Pages.paginate(success, pages, 60, TimeUnit.SECONDS, 2) }
         LOGGER.info("Player Achievements Sent For {}!", args)
     }
 
-    private fun buildAchievementEmbeds(player: Player, pages: ArrayList<Page>, ctx: CommandContext, typeOfAchievement: String) {
+    private fun buildAchievementEmbeds(
+        player: Player,
+        pages: ArrayList<Page>,
+        ctx: CommandContext,
+        typeOfAchievement: String
+    ) {
         when (typeOfAchievement.toLowerCase()) {
             "general" -> {
                 player.achievements.general.forEachIndexed { index, achievement ->
                     run {
-                        pages.add(Page(PageType.EMBED, getAchievementEmbed(player, achievement, ctx, index, player.achievements.general.size)))
+                        pages.add(
+                            Page(
+                                PageType.EMBED,
+                                getAchievementEmbed(player, achievement, ctx, index, player.achievements.general.size)
+                            )
+                        )
                     }
                 }
             }
             "damage" -> {
                 player.achievements.damage.forEachIndexed { index, achievement ->
                     run {
-                        pages.add(Page(PageType.EMBED, getAchievementEmbed(player, achievement, ctx, index, player.achievements.damage.size)))
+                        pages.add(
+                            Page(
+                                PageType.EMBED,
+                                getAchievementEmbed(player, achievement, ctx, index, player.achievements.damage.size)
+                            )
+                        )
                     }
                 }
             }
             "map" -> {
                 player.achievements.maps.forEachIndexed { index, achievement ->
                     run {
-                        pages.add(Page(PageType.EMBED, getAchievementEmbed(player, achievement, ctx, index, player.achievements.maps.size)))
+                        pages.add(
+                            Page(
+                                PageType.EMBED,
+                                getAchievementEmbed(player, achievement, ctx, index, player.achievements.maps.size)
+                            )
+                        )
                     }
                 }
             }
             "special" -> {
                 player.achievements.special.forEachIndexed { index, achievement ->
                     run {
-                        pages.add(Page(PageType.EMBED, getAchievementEmbed(player, achievement, ctx, index, player.achievements.special.size)))
+                        pages.add(
+                            Page(
+                                PageType.EMBED,
+                                getAchievementEmbed(player, achievement, ctx, index, player.achievements.special.size)
+                            )
+                        )
                     }
                 }
             }
             "support" -> {
                 player.achievements.support.forEachIndexed { index, achievement ->
                     run {
-                        pages.add(Page(PageType.EMBED, getAchievementEmbed(player, achievement, ctx, index, player.achievements.support.size)))
+                        pages.add(
+                            Page(
+                                PageType.EMBED,
+                                getAchievementEmbed(player, achievement, ctx, index, player.achievements.support.size)
+                            )
+                        )
                     }
                 }
             }
             "tank" -> {
                 player.achievements.tank.forEachIndexed { index, achievement ->
                     run {
-                        pages.add(Page(PageType.EMBED, getAchievementEmbed(player, achievement, ctx, index, player.achievements.tank.size)))
+                        pages.add(
+                            Page(
+                                PageType.EMBED,
+                                getAchievementEmbed(player, achievement, ctx, index, player.achievements.tank.size)
+                            )
+                        )
                     }
                 }
             }
@@ -84,58 +120,94 @@ class AchievementsInfo : ICommand {
 
     private fun getBaseEmbed(player: Player, ctx: CommandContext, pageNum: Int, totalPages: Int): EmbedBuilder {
         return EmbedBuilder()
-                .setAuthor(player.username + " | Level: " + player.level.value + " | Page: " + (pageNum + 1) + "/" + totalPages)
-                .setColor(Color.YELLOW)
-                .setTimestamp(Date().toInstant())
-                .setFooter("Powered By Swagger", ctx.selfUser.avatarUrl)
+            .setAuthor(player.username + " | Level: " + player.level.value + " | Page: " + (pageNum + 1) + "/" + totalPages)
+            .setColor(Color.YELLOW)
+            .setTimestamp(Date().toInstant())
+            .setFooter("Powered By Swagger", ctx.selfUser.avatarUrl)
     }
 
-    private fun getAchievementEmbed(player: Player, general: General, ctx: CommandContext, pageNum: Int, totalPages: Int): MessageEmbed {
+    private fun getAchievementEmbed(
+        player: Player,
+        general: General,
+        ctx: CommandContext,
+        pageNum: Int,
+        totalPages: Int
+    ): MessageEmbed {
         return getBaseEmbed(player, ctx, pageNum, totalPages)
-                .setTitle("General Achievements")
-                .setThumbnail(general.image)
-                .addField(general.title, general.description, true)
-                .build()
+            .setTitle("General Achievements")
+            .setThumbnail(general.image)
+            .addField(general.title, general.description, true)
+            .build()
     }
 
-    private fun getAchievementEmbed(player: Player, damage: Damage, ctx: CommandContext, pageNum: Int, totalPages: Int): MessageEmbed {
+    private fun getAchievementEmbed(
+        player: Player,
+        damage: Damage,
+        ctx: CommandContext,
+        pageNum: Int,
+        totalPages: Int
+    ): MessageEmbed {
         return getBaseEmbed(player, ctx, pageNum, totalPages)
-                .setTitle("Damage Achievements")
-                .setThumbnail(damage.image)
-                .addField(damage.title, damage.description, true)
-                .build()
+            .setTitle("Damage Achievements")
+            .setThumbnail(damage.image)
+            .addField(damage.title, damage.description, true)
+            .build()
     }
 
-    private fun getAchievementEmbed(player: Player, map: Map, ctx: CommandContext, pageNum: Int, totalPages: Int): MessageEmbed {
+    private fun getAchievementEmbed(
+        player: Player,
+        map: Map,
+        ctx: CommandContext,
+        pageNum: Int,
+        totalPages: Int
+    ): MessageEmbed {
         return getBaseEmbed(player, ctx, pageNum, totalPages)
-                .setTitle("Map Achievements")
-                .setThumbnail(map.image)
-                .addField(map.title, map.description, true)
-                .build()
+            .setTitle("Map Achievements")
+            .setThumbnail(map.image)
+            .addField(map.title, map.description, true)
+            .build()
     }
 
-    private fun getAchievementEmbed(player: Player, special: Special, ctx: CommandContext, pageNum: Int, totalPages: Int): MessageEmbed {
+    private fun getAchievementEmbed(
+        player: Player,
+        special: Special,
+        ctx: CommandContext,
+        pageNum: Int,
+        totalPages: Int
+    ): MessageEmbed {
         return getBaseEmbed(player, ctx, pageNum, totalPages)
-                .setTitle("Special Achievements")
-                .setThumbnail(special.image)
-                .addField(special.title, special.description, true)
-                .build()
+            .setTitle("Special Achievements")
+            .setThumbnail(special.image)
+            .addField(special.title, special.description, true)
+            .build()
     }
 
-    private fun getAchievementEmbed(player: Player, support: Support, ctx: CommandContext, pageNum: Int, totalPages: Int): MessageEmbed {
+    private fun getAchievementEmbed(
+        player: Player,
+        support: Support,
+        ctx: CommandContext,
+        pageNum: Int,
+        totalPages: Int
+    ): MessageEmbed {
         return getBaseEmbed(player, ctx, pageNum, totalPages)
-                .setTitle("Support Achievements")
-                .setThumbnail(support.image)
-                .addField(support.title, support.description, true)
-                .build()
+            .setTitle("Support Achievements")
+            .setThumbnail(support.image)
+            .addField(support.title, support.description, true)
+            .build()
     }
 
-    private fun getAchievementEmbed(player: Player, tank: Tank, ctx: CommandContext, pageNum: Int, totalPages: Int): MessageEmbed {
+    private fun getAchievementEmbed(
+        player: Player,
+        tank: Tank,
+        ctx: CommandContext,
+        pageNum: Int,
+        totalPages: Int
+    ): MessageEmbed {
         return getBaseEmbed(player, ctx, pageNum, totalPages)
-                .setTitle("Tank Achievements")
-                .setThumbnail(tank.image)
-                .addField(tank.title, tank.description, true)
-                .build()
+            .setTitle("Tank Achievements")
+            .setThumbnail(tank.image)
+            .addField(tank.title, tank.description, true)
+            .build()
     }
 
     override fun getName(): String {
@@ -143,11 +215,11 @@ class AchievementsInfo : ICommand {
     }
 
     override fun getHelp(): String {
-        return "Returns a list of achievements that the player has"
+        return "*[DEPRECATED]* Returns a list of achievements that the player has"
     }
 
     override fun getUsage(): String {
-        return "`!achievement <type> <platform> <region> <playerName>`"
+        return "*[DEPRECATED]*"
     }
 
     override fun getAliases(): List<String> {
