f1516d2cc8313eb519c7dc1d749f41000156a1ed
Hammy <sgoudham@gmail.com>
2021-04-28 22:18:45 +0100

    Rename package

--- src/main/java/exception/PlayerNotFoundException.java
+++ src/main/java/exception/PlayerNotFoundException.java
@@ -1,9 +1,6 @@
-package exceptions;
+package exception;
 
 public class PlayerNotFoundException extends Exception {
-
-    private static final long serialVersionUID = 6421798437058703463L;
-
     public PlayerNotFoundException(String message) {
         super(message);
     }
