57ee03151dceeb7d10a137e78d08b332ea39cbe7
Hammy <sgoudham@gmail.com>
2021-05-10 01:52:10 +0100

    Add Enum for winstonVoiceLines.txt

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -4,7 +4,8 @@ import java.nio.file.Paths;
 
 public enum Constants {
     RESOURCES_PATH(Paths.get("src", "main", "resources").toString()),
-    WINSTON_WEDNESDAY(Paths.get(RESOURCES_PATH.toString(), "winstonWednesday.mp4").toString());
+    WINSTON_WEDNESDAY(Paths.get(RESOURCES_PATH.toString(), "winstonWednesday.mp4").toString()),
+    WINSTON_VOICE_LINES(Paths.get(RESOURCES_PATH.toString(), "winstonVoiceLines.txt").toString());
 
     private final String constant;
 
