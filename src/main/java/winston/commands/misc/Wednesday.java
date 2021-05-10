909dcfe9d3d5a57c37827ee290936b23ccdaa9e7
Hammy <sgoudham@gmail.com>
2021-05-10 01:51:23 +0100

    Use Winston_Wednesday Enum

--- src/main/java/winston/commands/misc/Wednesday.java
+++ src/main/java/winston/commands/misc/Wednesday.java
@@ -5,11 +5,10 @@ import command.ICommand;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.io.File;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
-import static util.Constants.RESOURCES_PATH;
+import static util.Constants.WINSTON_WEDNESDAY;
 
 public class Wednesday implements ICommand {
 
@@ -17,7 +16,7 @@ public class Wednesday implements ICommand {
     public void handle(CommandContext ctx) throws Exception {
         TextChannel textChannel = ctx.getChannel();
         String messageBody = "**It's ~~White Girl~~ Winston Wednesday!**";
-        String winstonWednesdayPath = Paths.get(RESOURCES_PATH.toString(), "winstonWednesday.mp4").toString();
+        String winstonWednesdayPath = WINSTON_WEDNESDAY.toString();
 
         textChannel.sendMessage(messageBody).addFile(new File(winstonWednesdayPath)).queue();
     }
