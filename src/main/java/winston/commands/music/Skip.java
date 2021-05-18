bf47460fdb0a420cef3211b51fa67a75d27c64f4
Hammy <sgoudham@gmail.com>
2021-05-18 05:34:17 +0100

    Add skip command

---
+++ src/main/java/winston/commands/music/Skip.java
@@ -0,0 +1,71 @@
+package winston.commands.music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import command.CommandContext;
+import command.ICommand;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+import winston.commands.music.util.GuildMusicManager;
+import winston.commands.music.util.PlayerManager;
+
+import java.util.Collections;
+import java.util.List;
+
+import static winston.commands.music.util.Validation.*;
+
+@SuppressWarnings("ConstantConditions")
+public class Skip implements ICommand {
+
+    @Override
+    public void handle(CommandContext ctx) throws Exception {
+        TextChannel textChannel = ctx.getChannel();
+        Member bot = ctx.getSelfMember();
+        Member author = ctx.getMember();
+        GuildVoiceState authorVoiceState = author.getVoiceState();
+        GuildVoiceState botVoiceState = bot.getVoiceState();
+        GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(ctx.getGuild());
+        AudioPlayer audioPlayer = musicManager.getAudioPlayer();
+
+        if (botNotInVoiceChannel(botVoiceState, textChannel) || memberNotInVoiceChannel(authorVoiceState, textChannel)
+                || bothPartiesInDiffVoiceChannels(botVoiceState, authorVoiceState, textChannel) || noTrackPlaying(audioPlayer, textChannel)) {
+            return;
+        }
+
+        musicManager.scheduler.nextTrack();
+        textChannel.sendMessage("Skipped Current Track").queue();
+
+        if (!musicManager.scheduler.queue.isEmpty()) {
+            AudioTrack track = audioPlayer.getPlayingTrack();
+            AudioTrackInfo trackInfo = track.getInfo();
+            String title = trackInfo.title;
+            String artist = trackInfo.author;
+            String url = trackInfo.uri;
+
+            textChannel.sendMessage("Now Playing `" + title + "` by `" + artist + "` \nLink: <" + url + ">").queue();
+        }
+
+    }
+
+    @Override
+    public String getName() {
+        return "skip";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Skips the current song";
+    }
+
+    @Override
+    public String getUsage() {
+        return null;
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Collections.singletonList("next");
+    }
+}
