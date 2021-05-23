a25f59cfd7544ef2277681658a2d23b5fc62a53f
Hammy <sgoudham@gmail.com>
2021-05-23 07:15:49 +0100

    Refactor code Add variable for previous embed messageID Use event handlers for displaying embeds

--- src/main/java/winston/commands/music/util/TrackScheduler.java
+++ src/main/java/winston/commands/music/util/TrackScheduler.java
@@ -4,39 +4,68 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+import command.CommandContext;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
+import winston.bot.config.Logger;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static winston.commands.music.common.Common.buildSimpleInfo;
+import static winston.commands.music.common.Display.*;
 
 public class TrackScheduler extends AudioEventAdapter {
     private final AudioPlayer player;
     private final BlockingDeque<AudioTrack> deque;
+    private CommandContext ctx;
+    private AtomicReference<Long> previousEmbedMessageId;
+
     private boolean repeating = false;
     private boolean readyToPlayFile = false;
     private AudioTrack currentTack;
     private Long currentPos;
 
-    TrackScheduler(AudioPlayer player) {
+    TrackScheduler(AudioPlayer player, CommandContext ctx) {
         this.player = player;
+        this.ctx = ctx;
+
+        previousEmbedMessageId = null;
         deque = new LinkedBlockingDeque<>();
     }
 
-    public void queue(AudioTrack track, boolean isLocalFile) {
-        if (!player.startTrack(track, true)) {
-            if (isLocalFile) {
-                deque.addFirst(track);
-                setReadyToPlayFile(true);
-            } else {
-                deque.offer(track);
-            }
+    @Override
+    public void onPlayerPause(AudioPlayer player) { displayPausing(ctx, player); }
+
+    @Override
+    public void onPlayerResume(AudioPlayer player) {
+        displayResuming(ctx, player);
+    }
+
+    @Override
+    public void onTrackStart(AudioPlayer player, AudioTrack track) {
+        if (previousEmbedMessageId != null) {
+            ctx.getChannel().deleteMessageById(previousEmbedMessageId.get()).queue(message -> {},
+                    failure -> {
+                        if (failure instanceof ErrorResponseException) {
+                            ErrorResponseException ex = (ErrorResponseException) failure;
+                            Logger.LOGGER.info(ex.getMessage());
+                        }
+            });
         }
+        previousEmbedMessageId = displayNowPlaying(ctx, player);
     }
 
     @Override
     public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+        if (deque.isEmpty()) {
+            ctx.getChannel().sendMessage(buildSimpleInfo("End of Queue!", Color.YELLOW)).queue();
+        }
+
         if (endReason.mayStartNext) {
             if (isRepeating()) {
                 this.player.startTrack(track.makeClone(), false);
@@ -44,11 +73,22 @@ public class TrackScheduler extends AudioEventAdapter {
                 this.player.startTrack(currentTack.makeClone(), false);
                 this.player.getPlayingTrack().setPosition(currentPos);
 
-                setCurrentTack(null);
-                setCurrentPos(null);
-                setReadyToPlayFile(false);
+                currentTack = null;
+                currentPos = null;
+                readyToPlayFile = false;
             } else {
                 nextTrack();
+            }
+        }
+    }
+
+    public void queue(AudioTrack track, boolean isLocalFile) {
+        if (!player.startTrack(track, true)) {
+            if (isLocalFile) {
+                deque.addFirst(track);
+                readyToPlayFile = true;
+            } else {
+                deque.offer(track);
             }
         }
     }
@@ -57,26 +97,27 @@ public class TrackScheduler extends AudioEventAdapter {
         player.startTrack(deque.poll(), false);
     }
 
-    public void removeTrack() {
-        deque.removeFirst();
+    public AudioTrack removeTrack() {
+        return deque.removeFirst();
     }
 
     public void shuffle() {
         List<AudioTrack> trackList = getQueueAsList();
         Collections.shuffle(trackList);
-        setListAsQueue(trackList);
+        setListAsDeque(trackList);
     }
 
     public void skipTo(int index) {
         List<AudioTrack> trackList = getQueueAsList();
         trackList.subList(0, index).clear();
-        setListAsQueue(trackList);
+        setListAsDeque(trackList);
     }
 
-    public void removeTrack(int index) {
+    public AudioTrack removeTrack(int index) {
         List<AudioTrack> trackList = getQueueAsList();
-        trackList.remove(index - 1);
-        setListAsQueue(trackList);
+        AudioTrack removedTrack = trackList.remove(index);
+        setListAsDeque(trackList);
+        return removedTrack;
     }
 
     private List<AudioTrack> getQueueAsList() {
@@ -85,9 +126,10 @@ public class TrackScheduler extends AudioEventAdapter {
         return trackList;
     }
 
-    private void setListAsQueue(List<AudioTrack> trackList) {
+    private void setListAsDeque(List<AudioTrack> trackList) {
         deque.addAll(trackList);
     }
+
     public void setRepeating(boolean repeating) {
         this.repeating = repeating;
     }
@@ -115,7 +157,9 @@ public class TrackScheduler extends AudioEventAdapter {
     public boolean isReadyToPlayFile() {
         return readyToPlayFile;
     }
-    private void setReadyToPlayFile(boolean readyToPlayFile) {
+    public void setReadyToPlayFile(boolean readyToPlayFile) {
         this.readyToPlayFile = readyToPlayFile;
     }
+    public void setCtx(CommandContext ctx) { this.ctx = ctx; }
+    public CommandContext getCtx() { return ctx; }
 }
