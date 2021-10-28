c69d9e36265a21e535fb6d8b5fafa5a500c2e7d8
Hammy <sgoudham@gmail.com>
2021-10-28 03:31:47 +0100

    Defer reply and ensure that input stream is not reused

--- src/main/java/me/goudham/winston/bot/command/fun/WinstonWednesday.java
+++ src/main/java/me/goudham/winston/bot/command/fun/WinstonWednesday.java
@@ -5,17 +5,21 @@ import io.micronaut.core.io.ResourceLoader;
 import io.micronaut.core.io.scan.DefaultClassPathResourceLoader;
 import me.goudham.winston.command.annotation.SlashCommand;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 
 import java.io.InputStream;
 
 @SlashCommand(name = "winstonwednesday", description = "Display the very epic winston wednesday video")
 public class WinstonWednesday {
     private final ResourceLoader resourceLoader = new DefaultClassPathResourceLoader(ClassLoader.getSystemClassLoader());
-    private final InputStream resource = resourceLoader.getResourceAsStream("classpath:winstonWednesday.mp4").orElseThrow();
 
     @Executable
     public void handle(SlashCommandEvent slashCommandEvent) {
-        slashCommandEvent.reply("**It's ~~White Girl~~ Winston Wednesday!**")
+        slashCommandEvent.deferReply().queue();
+        InteractionHook hook = slashCommandEvent.getHook();
+        InputStream resource = resourceLoader.getResourceAsStream("classpath:winstonWednesday.mp4").orElseThrow();
+
+        hook.sendMessage("**It's ~~White Girl~~ Winston Wednesday!**")
                 .addFile(resource, "Winston.mp4")
                 .queue();
     }
